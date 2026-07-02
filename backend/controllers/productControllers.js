e2cd8cd7aedae58fa606db4f2f5f38120190b7f6
Muhammad Imran Khan <m9546618@gmail.com>
2026-07-02 16:27:28 +0500

    Learned about different types of error handling

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,25 +1,27 @@
+import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import Product from "../models/product.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
-// Get all products Product   =>  /api/v1/products
-export const getProducts = async (req, res) => {
+// Create new Product   =>  /api/v1/products
+export const getProducts = catchAsyncErrors(async (req, res) => {
   const products = await Product.find();
 
   res.status(200).json({
     products,
   });
-};
+});
 
 // Create new Product   =>  /api/v1/admin/products
-export const newProduct = async (req, res) => {
+export const newProduct = catchAsyncErrors(async (req, res) => {
   const product = await Product.create(req.body);
 
   res.status(200).json({
     product,
   });
-};
+});
 
 // Get single product details   =>  /api/v1/products/:id
-export const getProductDetails = async (req, res, next) => {
+export const getProductDetails = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req?.params?.id);
 
   if (!product) {
@@ -29,10 +31,10 @@ export const getProductDetails = async (req, res, next) => {
   res.status(200).json({
     product,
   });
-};
+});
 
 // Update product details   =>  /api/v1/products/:id
-export const updateProduct = async (req, res) => {
+export const updateProduct = catchAsyncErrors(async (req, res) => {
   let product = await Product.findById(req?.params?.id);
 
   if (!product) {
@@ -46,10 +48,10 @@ export const updateProduct = async (req, res) => {
   res.status(200).json({
     product,
   });
-};
+});
 
 // Delete product   =>  /api/v1/products/:id
-export const deleteProduct = async (req, res) => {
+export const deleteProduct = catchAsyncErrors(async (req, res) => {
   const product = await Product.findById(req?.params?.id);
 
   if (!product) {
@@ -61,4 +63,4 @@ export const deleteProduct = async (req, res) => {
   res.status(200).json({
     message: "Product Deleted",
   });
-};
+});
