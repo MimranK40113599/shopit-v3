e87a2e83b69d6464a4f94953d717569e6348b2fc
Muhammad Imran Khan <m9546618@gmail.com>
2026-07-03 20:30:29 +0500

    Learned how the Search, filter and pagination work

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(errorMiddleware);
 
 const server = app.listen(process.env.PORT, () => {
   console.log(
-    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`,
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
   );
 });
 
